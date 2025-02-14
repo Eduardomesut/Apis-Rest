974158d0db200343035c0b4ccd6d12f014b2cd04
Eduardomesut <eduardomesut@gmail.com>
2025-02-14 11:40:17 +0100

    Api con primos

--- Api-maths/src/main/java/com/mates/dao/HacerOperacion.java
+++ Api-maths/src/main/java/com/mates/dao/HacerOperacion.java
@@ -35,4 +35,12 @@ public class HacerOperacion {
         return this.operaciones;       
     }
     
+    public synchronized boolean esPrimo(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+    
 }
